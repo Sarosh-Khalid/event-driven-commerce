67d9a2d085cdbe8322d72e8edd381296937df0b3
msarosh <msarosh@eddress.co>
2026-06-25 02:33:43 +0500

    kfka confi added in payment-service

---
+++ services/payment-service/src/main/java/com/techvista/paymentservice/config/KafkaConfig.java
@@ -0,0 +1,91 @@
+package com.techvista.paymentservice.config;
+
+import com.techvista.paymentservice.kafka.InventoryUpdatedEvent;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+
+
+    @Bean
+    public ConsumerFactory<String, InventoryUpdatedEvent> consumerFactory(){
+
+
+        Map<String,Object> config = new HashMap<>();
+
+
+        config.put(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "localhost:9092"
+        );
+
+
+        config.put(
+                ConsumerConfig.GROUP_ID_CONFIG,
+                "payment-group"
+        );
+
+
+        config.put(
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class
+        );
+
+
+        config.put(
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                JsonDeserializer.class
+        );
+
+        JsonDeserializer<InventoryUpdatedEvent> deserializer =
+                new JsonDeserializer<>(
+                        InventoryUpdatedEvent.class
+                );
+        deserializer.addTrustedPackages("*");
+
+
+        return new DefaultKafkaConsumerFactory<>(
+                config,
+                new StringDeserializer(),
+                deserializer
+        );
+
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String,InventoryUpdatedEvent>
+    kafkaListenerContainerFactory(){
+
+
+        var factory =
+                new ConcurrentKafkaListenerContainerFactory
+
+
+
+
+
+                        <String,InventoryUpdatedEvent>();
+
+
+        factory.setConsumerFactory(
+                consumerFactory()
+        );
+
+
+        return factory;
+
+    }
+
+}
